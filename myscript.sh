a4562a06e2e3d9c6cf5499f5d8dd18c318f656e9
Fecovagyok <78788468+Fecovagyok@users.noreply.github.com>
2021-09-08 14:52:40 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -69,7 +69,7 @@ arch-chroot /mnt hwclock --systohc
 
 # uncommenting locales
 sed -i 's/#hu_HU.UTF-8 UTF-8/hu_HU.UTF-8 UTF-8/ ; s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8 UTF-8" > /mnt/etc/locale.conf
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 cat >> /mnt/etc/vconsole.conf << EOF
 KEYMAP=hu
 FONT=lat2-16
